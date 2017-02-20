7aaca9a4970772d6ac3641c6b49837f3a8e3913e
J David Smith <emallson@atlanis.net>
2017-02-20 09:32:36 -0500

    Added bash version of start.bat

---
+++ start.sh
@@ -0,0 +1,13 @@
+java PeerProcess 1001 &
+sleep 1
+java PeerProcess 1002 &
+sleep 1
+java PeerProcess 1003 &
+sleep 1
+java PeerProcess 1004 &
+sleep 1
+java PeerProcess 1005 &
+sleep 1
+java PeerProcess 1006
+
+pkill -P $$
