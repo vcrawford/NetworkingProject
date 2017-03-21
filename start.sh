32eae4890c6b86eaed09ff1eee08a6fec6d0561f
vcrawford <vcrawford01@gmail.com>
2017-03-21 10:04:20 -0400

    Log individual peers

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
-java -Dcolor -jar build/libs/NetworkingProject-all.jar 1001 &
+java -Dcolor -Dpeerid=1001 -jar build/libs/NetworkingProject-all.jar 1001 &
 sleep 1
-java -Dcolor -jar build/libs/NetworkingProject-all.jar 1002 &
+java -Dcolor -Dpeerid=1002 -jar build/libs/NetworkingProject-all.jar 1002 &
 sleep 1
-java -Dcolor -jar build/libs/NetworkingProject-all.jar 1003 &
+java -Dcolor -Dpeerid=1003 -jar build/libs/NetworkingProject-all.jar 1003 &
 sleep 1
-java -Dcolor -jar build/libs/NetworkingProject-all.jar 1004 &
+java -Dcolor -Dpeerid=1004 -jar build/libs/NetworkingProject-all.jar 1004 &
 sleep 1
-java -Dcolor -jar build/libs/NetworkingProject-all.jar 1005 &
+java -Dcolor -Dpeerid=1005 -jar build/libs/NetworkingProject-all.jar 1005 &
 sleep 1
-java -Dcolor -jar build/libs/NetworkingProject-all.jar 1006
+java -Dcolor -Dpeerid=1006 -jar build/libs/NetworkingProject-all.jar 1006
 
 pkill -P $$
